cce876d99e2dee736b4d40f3bfedeaf0b89d0b18
Madadi youcef <tahher@live.fr>
2022-05-13 23:30:08 +0100

    Update AppWebsiteVisits.js

--- src/sections/@dashboard/app/AppWebsiteVisits.js
+++ src/sections/@dashboard/app/AppWebsiteVisits.js
@@ -38,7 +38,6 @@ export default function AppWebsiteVisits({ title, subheader, chartLabels, chartD
   return (
     <Card {...other}>
       <CardHeader title={title} subheader={subheader} />
-
       <Box sx={{ p: 3, pb: 1 }} dir="ltr">
         <ReactApexChart type="line" series={chartData} options={chartOptions} height={364} />
       </Box>
